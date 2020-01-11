f9fda7d9c7cbebe2c09858babace90f84c877506
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2020-01-11 06:05:00 -0300

    update

---
+++ formater.js
@@ -0,0 +1,68 @@
+const author = {
+	name: 'Francisco',
+	lastname: 'Rodriguez'
+}
+
+const formatPrice = (currency, price) => {
+	if (/\./.test(String(price))) {
+		return {
+			currency,
+			amount: Number(String(price).split('.')[0]),
+			decimals: Number(String(price).split('.')[1])
+		}
+	} else {
+		return {
+			currency,
+			amount: price,
+			decimals: 0
+		}
+	}
+}
+
+const formatItems = ({ available_filters, filters, results }) => {
+	const categories = () => {
+		let categoriesFromAvailableFilters = available_filters.filter(
+			filter => filter.id === 'category'
+		)
+		let categoriesFromFilters = filters.filter(
+			filter => filter.id === 'category'
+		)
+		if (categoriesFromAvailableFilters.length > 0) {
+			return categoriesFromAvailableFilters[0].values.map(({ name }) => name)
+		} else if (categoriesFromFilters.length > 0) {
+			return categoriesFromFilters[0].values.map(({ name }) => name)
+		}
+	}
+
+	const items = results.map(result => ({
+		id: result.id,
+		title: result.title,
+		price: formatPrice(result.currency_id, result.price),
+		picture: result.thumbnail.replace('http', 'https'),
+		condition: result.condition,
+		free_shipping: result.shipping.free_shipping,
+		city: result.address.state_name
+	}))
+
+	return { author, categories: categories(), items }
+}
+
+const formatItem = (product, description, categories) => {
+	const paths = categories.path_from_root.map(path => path.name)
+
+	const item = {
+		id: product.id,
+		title: product.title,
+		price: formatPrice(product.currency_id, product.price),
+		picture: product.pictures[0].url.replace('http', 'https'),
+		condition: product.condition,
+		free_shipping: product.shipping.free_shipping,
+		sold_quantity: product.sold_quantity,
+		description: description[0].plain_text,
+		categories: paths
+	}
+
+	return { author, item }
+}
+
+module.exports = { formatItems, formatItem }
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const path = require('path')
 const compression = require('compression')
-const async = require('express-async-await')
 const fetch = require('node-fetch')
 const port = process.env.PORT || 5000
 const app = express()
+const proxy = 'https://api.mercadolibre.com'
+const { formatItems, formatItem } = require('./formater')
 
 app.use(compression())
 app.use(function(req, res, next) {
@@ -25,20 +25,16 @@ app.use(function(req, res, next) {
 	next()
 })
 
-app.get('/api/trends', async function(req, res) {
+app.get('/api/trends', async (req, res) => {
 	try {
-		const response = await fetch('https://api.mercadolibre.com/trends/MLA', {
+		const response = await fetch(`${proxy}/trends/MLA`, {
 			headers: { 'Accept-Encoding': 'br' }
 		})
 		const data = await response.json()
-		const trends = await data.map(trend => {
-			let item = {
-				keyword: trend.keyword,
-				url: trend.url
-			}
-			return item
-		})
-		res.json({ trends: trends })
+
+		const trends = await data.map(({ keyword }) => keyword)
+
+		res.json({ trends })
 	} catch {
 		res
 			.status(404)
@@ -46,70 +42,16 @@ app.get('/api/trends', async function(req, res) {
 	}
 })
 
-app.get('/api/items', async function(req, res) {
+app.get('/api/items', async ({ query: { q } }, res) => {
 	try {
-		const response = await fetch(
-			`https://api.mercadolibre.com/sites/MLA/search?q=${req.query.q}&limit=4`
-		)
+		const response = await fetch(`${proxy}/sites/MLA/search?q=${q}&limit=4`)
 		const data = await response.json()
 
 		if (data.results.length === 0) {
-			res.status(204).send(`No se obtubieron resultados de ${req.query.q}`)
+			res.status(204).send(`No se obtubieron resultados de ${q}`)
 		} else {
-			const categories = await data.available_filters[0].values.map(
-				category => category.name
-			)
-			// let categories = []
-
-			// let categoriesFromAvailableFilters = await data.available_filters.filter(
-			// 	filter => filter.id === 'category'
-			// )
-			// let categoriesFromFilters = await data.filters.filter(
-			// 	filter => filter.id === 'category'
-			// )
-
-			// if (categoriesFromAvailableFilters.length > 0) {
-			// 	categories = categoriesFromAvailableFilters[0].values.map(
-			// 		({ name }) => name
-			// 	)
-			// } else if (categoriesFromFilters.length > 0) {
-			// 	categories = categoriesFromFilters[0].values.map(({ name }) => name)
-			// }
-
-			const items = await data.results.map(result => {
-				let formatPrice = {}
-				if (/\./.test(String(result.price))) {
-					formatPrice.intPart = Number(String(result.price).split('.')[0])
-					formatPrice.decPart = Number(String(result.price).split('.')[1])
-				} else {
-					formatPrice.intPart = result.price
-					formatPrice.decPart = 0
-				}
-
-				let item = {
-					id: result.id,
-					title: result.title,
-					price: {
-						currency: result.currency_id,
-						amount: formatPrice.intPart,
-						decimals: formatPrice.decPart
-					},
-					picture: result.thumbnail.replace('http', 'https'),
-					condition: result.condition,
-					free_shipping: result.shipping.free_shipping,
-					city: result.address.state_name
-				}
-				return item
-			})
-			const final = {
-				author: {
-					name: 'Francisco',
-					lastname: 'Rodriguez'
-				},
-				categories: categories,
-				items: items
-			}
-			res.json(final)
+			const items = formatItems(data)
+			res.json(items)
 		}
 	} catch (error) {
 		res
@@ -118,52 +60,23 @@ app.get('/api/items', async function(req, res) {
 	}
 })
 
-app.get('/api/items/:id', async function(req, res) {
+app.get('/api/items/:id', async (req, res) => {
 	try {
 		const [productResponse, descriptionResponse] = await Promise.all([
-			fetch(`https://api.mercadolibre.com/items/${req.params.id}`),
-			fetch(`https://api.mercadolibre.com/items/${req.params.id}/descriptions`)
+			fetch(`${proxy}/items/${req.params.id}`),
+			fetch(`${proxy}/items/${req.params.id}/descriptions`)
 		])
 
 		const productData = await productResponse.json()
-
 		const descriptionData = await descriptionResponse.json()
 
 		const categoriesResponse = await fetch(
-			`https://api.mercadolibre.com/categories/${productData.category_id}`
+			`${proxy}/categories/${productData.category_id}`
 		)
 		const categoriesData = await categoriesResponse.json()
 
-		const paths = await categoriesData.path_from_root.map(path => path.name)
-
-		let formatPrice = {}
-		if (/\./.test(String(productData.price))) {
-			formatPrice.intPart = Number(String(productData.price).split('.')[0])
-			formatPrice.decPart = Number(String(productData.price).split('.')[1])
-		} else {
-			formatPrice.intPart = productData.price
-			formatPrice.decPart = 0
-		}
-
-		const product = {
-			author: { name: 'Francisco', lastname: 'Rodriguez' },
-			item: {
-				id: productData.id,
-				title: productData.title,
-				price: {
-					currency: productData.currency_id,
-					amount: formatPrice.intPart,
-					decimals: formatPrice.decPart
-				},
-				picture: productData.pictures[0].url.replace('http', 'https'),
-				condition: productData.condition,
-				free_shipping: productData.shipping.free_shipping,
-				sold_quantity: productData.sold_quantity,
-				description: descriptionData[0].plain_text,
-				categories: paths
-			}
-		}
-		res.json(product)
+		const item = formatItem(productData, descriptionData, categoriesData)
+		res.json(item)
 	} catch {
 		res
 			.status(404)
